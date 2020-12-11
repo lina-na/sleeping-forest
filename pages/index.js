6f568daec62ad8149b2d669b8b6dd50685ce54ff
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2020-12-11 19:38:36 +0300

    Add canvas element

--- pages/index.js
+++ pages/index.js
@@ -2,42 +2,20 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Sleeping Forest</title>
         <link rel="icon" href="/favicon.ico" />
+        {typeof window !== 'undefined' && <script src='/wow.js'></script>}
       </Head>
 
-      <main className={styles.main}>
+      <canvas id="wow"></canvas>
         <h1 className={styles.title}>Welcome to Sleeping Forest!</h1>
-
         <p className={styles.description}>Линча сейчас работает над сайтом.</p>
-
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h3>Супер мега вокалисты &rarr;</h3>
-            <p>Этот раздел скоро появится.</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3>Опупенные художники &rarr;</h3>
-            <p>Этот раздел скоро появится.</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3>Лучшие в мире переводчики &rarr;</h3>
-            <p>Этот раздел скоро появится.</p>
-          </div>
-
-          <div className={styles.card}>
-            <h3>Профессиональные виддеры и звуковики &rarr;</h3>
-            <p>Этот раздел скоро появится.</p>
-          </div>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>Жду от Вовы мокапы. ❤️</footer>
+  
+      <p className={styles.footer}>Жду от Вовы мокапы. ❤️</p>
     </div>
   )
 }
