e28d7e33ae736b731eb4e37a3f1a99db19e51a1e
Lina Aleksandrova <ang.s.alexandrova@gmail.com>
2020-12-12 18:00:16 +0300

    Update the way the script is imported

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/home.module.css'
+import { useScripts } from '../hooks/useScripts'
+import Wow from '../scripts/wow'
 
 export default function Home() {
-
+  useScripts([Wow])
   return (
     <div className={styles.container}>
       <Head>
         <title>Sleeping Forest</title>
         <link rel="icon" href="/favicon.ico" />
-        {typeof window !== 'undefined' && <script src='/wow.js'></script>}
       </Head>
 
       <canvas id="wow"></canvas>
-        <h1 className={styles.title}>Welcome to Sleeping Forest!</h1>
-        <p className={styles.description}>Линча сейчас работает над сайтом.</p>
-  
+      <h1 className={styles.title}>Welcome to Sleeping Forest!</h1>
+      <p className={styles.description}>Линча сейчас работает над сайтом.</p>
+
       <p className={styles.footer}>Жду от Вовы мокапы. ❤️</p>
     </div>
   )
