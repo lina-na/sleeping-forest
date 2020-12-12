fc3f79b4b95efaa5ca7bd3b389499964f7d17072
Lina Aleksandrova <ang.s.alexandrova@gmail.com>
2020-12-12 18:06:25 +0300

    Fix build error

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.css'
+import styles from '../styles/Home.module.css'
 import { useScripts } from '../hooks/useScripts'
 import Wow from '../scripts/wow'
 
