69fe10da494b72be09b57ea9891e7a58de02d1d5
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2021-01-20 17:09:17 +0300

    Add horizontal scroll

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,36 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useScripts } from '../hooks/useScripts'
 import Wow from '../scripts/wow'
+import { useEffect } from 'react'
+import HorizontalScroll from '../components/horizontal-scroll'
+
 
 export default function Home() {
   useScripts([Wow])
+  let width, height;
+  useEffect(() => {
+    width = typeof window !== 'undefined' && window.innerWidth
+    height = typeof window !== 'undefined' && window.innerHeight
+  }, [typeof window !== 'undefined' && window]);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.page}>
       <Head>
         <title>Sleeping Forest</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <HorizontalScroll>
+        <div className={styles.container}>
+          <canvas width={width} height={height} id="wow"></canvas>
+          <h1 className={styles.title}>Welcome to Sleeping Forest!</h1>
+          <p className={styles.description}>Линча сейчас работает над сайтом.</p>
 
-      <canvas id="wow"></canvas>
-      <h1 className={styles.title}>Welcome to Sleeping Forest!</h1>
-      <p className={styles.description}>Линча сейчас работает над сайтом.</p>
+          <p className={styles.footer}>Жду от Вовы мокапы. ❤️</p>
+        </div>
 
-      <p className={styles.footer}>Жду от Вовы мокапы. ❤️</p>
+        <div className={styles.container}>Kek</div>
+
+      </HorizontalScroll>
     </div>
   )
 }
