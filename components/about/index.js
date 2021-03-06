b902b167be37d3305a20ed05f5c1f42cfe887d6a
Lina <lina@creativehandles.com>
2021-03-06 16:38:41 +0300

    Do some things for the about page

--- components/about/index.js
+++ components/about/index.js
@@ -1,4 +1,42 @@
 import { useMemo } from 'react'
+import s from 'styled-components';
+
+const Background = s.div`
+  background-image: url("cover.jpg");
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  background-size: cover;
+`;
+
+const WaterflowWrapper = s.div`
+  max-width: 40%;
+`;
+
+const Button = s.button`
+  padding: 12px 41px 14px 40px;
+  border-radius: 20px;
+  background-color: #9ccb3b;
+  text-transform: uppercase;
+  transition: background-color .3s;
+
+  :hover {
+    background-color: #66920b;
+  }
+`;
+
+const AboutContent = s.div`
+  max-width: 30%;
+  margin-right: 50px;
+  align-self: center;
+
+  p { 
+    margin-bottom: 20px;
+  }
+`;
+
 const TYPE_PARTICLE = 'TYPE_PARTICLE';
 const TYPE_IMAGE = 'TYPE_IMAGE';
 
@@ -70,14 +108,30 @@ export default function About() {
   const shuffledArray = useMemo(() => shuffleArray([...particles, ...cards]), []);
 
   return (
-    <div>
-      <div className="section">
-        {typeof window !== 'undefined' && shuffledArray.map(({ type, size, name, backgroundImage }, index) =>
-          <div style={size && { width: size, height: size }} key={type + index + name || size} className={type === TYPE_IMAGE ? "img" : 'particle img'}>
-            {name && <h4>{name}</h4>}
-            {backgroundImage && <img src={backgroundImage} alt={name} />}
-          </div>)}
-      </div>
-    </div>
+    <Background>
+      <AboutContent>
+        <p>
+          Nunc quis neque tristique, porta ligula ac, lobortis leo.
+          Vivamus tincidunt vulputate dignissim. Sed bibendum ante justo, vitae finibus justo pellentesque ut. In gravida magna tristique, dictum augue eget, tristique odio. Duis id placerat enim. Fusce turpis elit, pulvinar vel dapibus sed, elementum ac mauris.
+        </p>
+        <p>
+          Cras molestie, nunc ut sollicitudin ultricies, purus leo commodo nunc, vel viverra metus arcu quis dolor.
+          Curabitur lorem arcu, volutpat vitae posuere nec, iaculis quis lacus. Vestibulum in mauris sed turpis posuere ultricies.
+          Suspendisse aliquet odio sit amet mauris euismod mollis. Ut sed odio eu neque blandit maximus a nec felis.
+          </p>
+        <br />
+        <Button>Узнать Больше</Button>
+      </AboutContent>
+
+      <WaterflowWrapper>
+        <div className="section">
+          {typeof window !== 'undefined' && shuffledArray.map(({ type, size, name, backgroundImage }, index) =>
+            <div style={size && { width: size, height: size }} key={type + index + name || size} className={type === TYPE_IMAGE ? "img" : 'particle img'}>
+              {name && <h4>{name}</h4>}
+              {backgroundImage && <img src={backgroundImage} alt={name} />}
+            </div>)}
+        </div>
+      </WaterflowWrapper>
+    </Background>
   )
 }
